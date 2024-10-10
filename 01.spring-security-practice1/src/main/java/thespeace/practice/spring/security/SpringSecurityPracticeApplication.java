f545a231e2327b6496126c2c747eb1e9726f95c6
thespeace <thespeace1@gmail.com>
2024-10-10 17:41:21 +0900

    initialize Spring Security Practice Project1

---
+++ 01.spring-security-practice1/src/main/java/thespeace/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -0,0 +1,14 @@
+package thespeace.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1>SpringSecurity 연습 Application</h1>
+ */
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
